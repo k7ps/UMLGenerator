b10e4476704b3bb60ce7fd606d87cb2c77ed8c87
murat <ibrashov.mb@phystech.edu>
2022-03-23 21:53:36 +0300

    Added Reader

---
+++ reader.py
@@ -0,0 +1,8 @@
+class Reader:
+    def read_code(path):
+        readed_code = []
+        with open(path, encoding='utf8') as f:
+            for i in f:
+                readed_code.append(i) # прочитали весь код построчно
+        return readed_code
+
